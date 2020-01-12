cb664c70bece433fe73325f2734ee6559f663295
abdellah <abdellah.essofyany@gmail.com>
2020-01-12 20:07:05 +0100

    add swagger doc api

--- src/person/person.ts
+++ src/person/person.ts
@@ -1,5 +1,12 @@
+import {ApiProperty} from '@nestjs/swagger';
+
 export default class Person {
+    @ApiProperty()
     name: string;
+
+    @ApiProperty()
     age: number;
+
+    @ApiProperty()
     dateOfBirth: Date;
 }
